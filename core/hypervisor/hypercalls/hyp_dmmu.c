b532651d4d7de94f31d605dd52e4b25c0a803ade
Viktor Do <viktordo@sics.se>
2014-05-13 16:06:59 +0200

    Flush and invalidate is now working on real hw without excessive world flush/invalidate

--- core/hypervisor/hypercalls/hyp_dmmu.c
+++ core/hypervisor/hypercalls/hyp_dmmu.c
@@ -97,11 +97,6 @@ void hypercall_dyn_free_pgd(addr_t * pgd_va)
 	}
 
 	hypercall_dcache_clean_area((uint32_t) pgd_va, 0x4000);
-#if 1
-	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
-#endif
 }
 
 /*New pages for processes, copys kernel space from master pages table
@@ -193,17 +188,18 @@ void hypercall_dyn_new_pgd(addr_t * pgd_va)
 		    (table2_pa - (table2_pa & L2_BASE_MASK)) >> MMU_L1_PT_SHIFT;
 		table2_idx *= 0x100;	/*256 pages per L2PT */
 
-		uint32_t l2_entry_idx = (((uint32_t) pgd_va << 12) >> 24);
+		uint32_t l2_entry_idx =
+		    (((uint32_t) pgd_va << 12) >> 24) + table2_idx;
 
 		uint32_t *l2_page_entry =
-		    (addr_t
-		     *) (mmu_guest_pa_to_va(table2_pa, (curr_vm->config)));
+		    (addr_t *) (mmu_guest_pa_to_va(table2_pa & L2_BASE_MASK,
+						   (curr_vm->config)));
 		uint32_t page_pa =
 		    MMU_L2_SMALL_ADDR(l2_page_entry[l2_entry_idx]);
 
-		i = table2_idx + l2_entry_idx;
-		end = i + 4;
-		for (; i < end; i++, page_pa += 0x1000) {
+		addr_t clean_va;
+		for (i = l2_entry_idx; i < l2_entry_idx + 4;
+		     i++, page_pa += 0x1000) {
 			if (dmmu_l2_unmap_entry(table2_pa & L2_BASE_MASK, i))
 				printf
 				    ("\n\tCould not unmap L2 entry in new PGD\n");
@@ -213,6 +209,17 @@ void hypercall_dyn_new_pgd(addr_t * pgd_va)
 			    (table2_pa & L2_BASE_MASK, i, page_pa, ro_attrs))
 				printf
 				    ("\n\tCould not map L2 entry in new pgd\n");
+
+			clean_va =
+			    LINUX_VA(MMU_L2_SMALL_ADDR(l2_page_entry[i]));
+			COP_WRITE(COP_SYSTEM, COP_DCACHE_INVALIDATE_MVA,
+				  &l2_page_entry[i]);
+			dsb();
+
+			COP_WRITE(COP_SYSTEM, COP_TLB_INVALIDATE_MVA, clean_va);
+			COP_WRITE(COP_SYSTEM, COP_BRANCH_PRED_INVAL_ALL, clean_va);	/*Update cache with new values */
+			dsb();
+			isb();
 		}
 	}
 
@@ -232,11 +239,6 @@ void hypercall_dyn_new_pgd(addr_t * pgd_va)
 	if (dmmu_create_L1_pt(LINUX_PA((addr_t) pgd_va))) {
 		printf("\n\tCould not create L1 pt in new pgd\n");
 	}
-#if 0
-	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
-#endif
 }
 
 /*In ARM linux pmd refers to pgd, ARM L1 Page table
@@ -477,11 +479,6 @@ void hypercall_dyn_set_pmd(addr_t * pmd, uint32_t desc)
 	/*Flush entry */
 	COP_WRITE(COP_SYSTEM, COP_DCACHE_INVALIDATE_MVA, (uint32_t) pmd);
 	dsb();
-#if 0
-	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
-#endif
 }
 
 /*va is the virtual address of the page table entry for linux pages
