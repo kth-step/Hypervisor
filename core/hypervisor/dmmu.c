eb904b303d27a87d55fcf3e2a62fbe7014609e26
Hamed Nemati <hnnemati@kth.se>
2015-05-31 14:20:46 +0200

    cache maintenance is fixed + cache attack integrity

--- core/hypervisor/dmmu.c
+++ core/hypervisor/dmmu.c
@@ -263,18 +263,19 @@ int dmmu_create_L1_pt(addr_t l1_base_pa_add)
 	uint32_t ph_block;
 	int i;
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
-
+	CacheDataCleanInvalidateAll();
+#endif
 	/*Check that the guest does not override the physical addresses outside its range */
 	// TODO, where we take the guest assigned physical memory?
 	if (!guest_pa_range_checker(l1_base_pa_add, 4 * PAGE_SIZE))
 		return ERR_MMU_OUT_OF_RANGE_PA;
 
-	if (!guest_pt_range_checker(l1_base_pa_add, 4 * PAGE_SIZE))
-		return ERR_MMU_OUT_OF_CACHEABLE_RANGE;
+	//printf("Hyper L1 base in L1 create %x \n", l1_base_pa_add);
+	//if (!guest_pt_range_checker(l1_base_pa_add, 4*PAGE_SIZE))
+	//  return ERR_MMU_OUT_OF_CACHEABLE_RANGE;
 
 	/* 16KB aligned ? */
 	if (l1_base_pa_add != (l1_base_pa_add & 0xFFFFC000))
@@ -348,10 +349,11 @@ int dmmu_create_L1_pt(addr_t l1_base_pa_add)
 	get_bft_entry_by_block_idx(ph_block + 2)->type = PAGE_INFO_TYPE_L1PT;
 	get_bft_entry_by_block_idx(ph_block + 3)->type = PAGE_INFO_TYPE_L1PT;
 
+#ifdef AGGRESSIVE_FLUSHINGV
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	return 0;
 }
@@ -370,10 +372,11 @@ uint32_t dmmu_map_L1_section(addr_t va, addr_t sec_base_add, uint32_t attrs)
 	uint32_t ap;
 	int sec_idx;
 
+#ifdef AGGRESSIVE_FLUSHINGV
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	/*Check that the guest does not override the virtual addresses used by the hypervisor */
 	// HAL_VIRT_START is usually 0xf0000000, where the hypervisor code/data structures reside
@@ -446,11 +449,11 @@ uint32_t dmmu_map_L1_section(addr_t va, addr_t sec_base_add, uint32_t attrs)
 		}
 		*((uint32_t *) l1_desc_va_add) = l1_desc;
 	}
-
+#ifdef AGGRESSIVE_FLUSHINGV
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	return 0;
 }
@@ -467,10 +470,11 @@ uint32_t dmmu_unmap_L1_pageTable_entry(addr_t va)
 	uint32_t l1_desc;
 	uint32_t l1_type;
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	/*Check that the guest does not override the virtual addresses used by the hypervisor */
 	// HAL_VIRT_START is usually 0xf0000000, where the hypervisor code/data structures reside
@@ -528,10 +532,11 @@ uint32_t dmmu_unmap_L1_pageTable_entry(addr_t va)
 		return ERR_MMU_ENTRY_UNMAPPED;
 	}
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	return 0;
 }
@@ -636,10 +641,11 @@ void create_L2_pgtype_update(uint32_t l2_base_pa_add)
 
 uint32_t dmmu_create_L2_pt(addr_t l2_base_pa_add)
 {
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	uint32_t l2_desc_pa_add;
 	uint32_t l2_desc_va_add;
@@ -655,8 +661,8 @@ uint32_t dmmu_create_L2_pt(addr_t l2_base_pa_add)
 		return ERR_MMU_OUT_OF_RANGE_PA;
 
 	//printf("Hyper L2 base in L2 create %x \n", l2_base_pa_add);
-	if (!guest_pt_range_checker(l2_base_pa_add, PAGE_SIZE))
-		return ERR_MMU_OUT_OF_CACHEABLE_RANGE;
+	//if (!guest_pt_range_checker(l2_base_pa_add, PAGE_SIZE))
+	//  return ERR_MMU_OUT_OF_CACHEABLE_RANGE;
 
 	//not 4KB aligned ?
 	if (l2_base_pa_add != (l2_base_pa_add & L2_BASE_MASK))
@@ -697,10 +703,11 @@ uint32_t dmmu_create_L2_pt(addr_t l2_base_pa_add)
 	} else
 		return ERR_MMU_SANITY_CHECK_FAILED;
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	return 0;
 }
@@ -717,10 +724,11 @@ int dmmu_l1_pt_map(addr_t va, addr_t l2_base_pa_add, uint32_t attrs)
 	uint32_t l1_desc;
 	uint32_t page_desc;
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	// HAL_VIRT_START is usually 0xf0000000, where the hypervisor code/data structures reside
 	/*Check that the guest does not override the virtual addresses used by the hypervisor */
@@ -774,11 +782,11 @@ int dmmu_l1_pt_map(addr_t va, addr_t l2_base_pa_add, uint32_t attrs)
 
 	*((uint32_t *) l1_desc_va_add) = l1_desc;
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
-
+	CacheDataCleanInvalidateAll();
+#endif
 	return 0;
 }
 
@@ -788,10 +796,11 @@ int dmmu_l1_pt_map(addr_t va, addr_t l2_base_pa_add, uint32_t attrs)
 int dmmu_l2_map_entry(addr_t l2_base_pa_add, uint32_t l2_idx,
 		      addr_t page_pa_add, uint32_t attrs)
 {
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	uint32_t l2_desc_pa_add;
 	uint32_t l2_desc_va_add;
@@ -847,10 +856,11 @@ int dmmu_l2_map_entry(addr_t l2_base_pa_add, uint32_t l2_idx,
 	l2_desc = CREATE_L2_DESC(page_pa_add, attrs);
 	*((uint32_t *) l2_desc_va_add) = l2_desc;
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	return 0;
 }
@@ -860,10 +870,11 @@ int dmmu_l2_map_entry(addr_t l2_base_pa_add, uint32_t l2_idx,
  *  -------------------------------------------------------------------*/
 int dmmu_l2_unmap_entry(addr_t l2_base_pa_add, uint32_t l2_idx)
 {
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	uint32_t l2_desc_pa_add;
 	uint32_t l2_desc_va_add;
@@ -901,10 +912,11 @@ int dmmu_l2_unmap_entry(addr_t l2_base_pa_add, uint32_t l2_idx)
 	l2_desc = UNMAP_L2_ENTRY(l2_desc);
 	*((uint32_t *) l2_desc_va_add) = l2_desc;
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	return 0;
 }
@@ -914,10 +926,11 @@ int dmmu_l2_unmap_entry(addr_t l2_base_pa_add, uint32_t l2_idx)
  *  -------------------------------------------------------------------*/
 int dmmu_unmap_L2_pt(addr_t l2_base_pa_add)
 {
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	uint32_t l2_desc_pa_add;
 	uint32_t l2_desc_va_add;
@@ -965,10 +978,11 @@ int dmmu_unmap_L2_pt(addr_t l2_base_pa_add)
 	//Changing the type of the L2 page table to data page
 	bft_entry->type = PAGE_INFO_TYPE_DATA;
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	return 0;
 }
@@ -979,10 +993,11 @@ int dmmu_unmap_L2_pt(addr_t l2_base_pa_add)
 //#define SW_DEBUG
 int dmmu_switch_mm(addr_t l1_base_pa_add)
 {
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	int i;
 	uint32_t ph_block;
@@ -1018,10 +1033,11 @@ int dmmu_switch_mm(addr_t l1_base_pa_add)
 	/* activate the guest page table */
 	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
 	COP_WRITE(COP_SYSTEM, COP_SYSTEM_TRANSLATION_TABLE0, l1_base_pa_add);	// Set TTB0
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 	return 0;
 }
 
@@ -1032,10 +1048,11 @@ int dmmu_switch_mm(addr_t l1_base_pa_add)
  *  ------------------------------------------------------------------- */
 int dmmu_unmap_L1_pt(addr_t l1_base_pa_add)
 {
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	uint32_t l1_idx, pt_idx, sec_idx;
 	uint32_t l1_desc;
@@ -1113,10 +1130,11 @@ int dmmu_unmap_L1_pt(addr_t l1_base_pa_add)
 	get_bft_entry_by_block_idx(ph_block + 2)->type = PAGE_INFO_TYPE_DATA;
 	get_bft_entry_by_block_idx(ph_block + 3)->type = PAGE_INFO_TYPE_DATA;
 
+#ifdef AGGRESSIVE_FLUSHING
 	isb();
 	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
-	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
-	mem_cache_set_enable(TRUE);
+	CacheDataCleanInvalidateAll();
+#endif
 
 	return 0;
 }
